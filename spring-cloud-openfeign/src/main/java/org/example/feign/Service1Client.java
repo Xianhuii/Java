ffb231d68eb82c112e319d6b6be78c7934dc5704
xianhuii <xianhuii@foxmail.com>
2023-08-27 11:52:52 +0800

    spring cloud

---
+++ spring-cloud-openfeign/src/main/java/org/example/feign/Service1Client.java
@@ -0,0 +1,10 @@
+package org.example.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient("service1")
+public interface Service1Client {  
+    @GetMapping("/test")
+    String test();  
+}
